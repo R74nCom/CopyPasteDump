bbdc82eadd12a56275db24fe5c694fd503fa0a01
slweeb <91897291+slweeb@users.noreply.github.com>
2023-03-17 12:14:36 -0400

    Revert "let there be emoji"
    This reverts commit a47f8ed5a47af8f7af6c82b4ec3170ccf2f62b3f.

--- asdfgh/adsjfhakhfjsd.py
+++
@@ -1,5 +0,0 @@
-text = open("text.txt","r",encoding="utf8").read()
-
-savefile = open("new.txt","w",encoding="utf8")
-savefile.write(text.replace("  "," &nbsp;").replace("<","&lt;").replace(">","&gt;").replace("\n","\n<br>"))
-savefile.close()
--- instagram/pridegen.py
+++
@@ -1,9 +0,0 @@
-l = ["gay","lgbt","lgbtq","lgbtqi","lgbtqia","lgbtqiap","lgbtplus","lgbtqplus","lgbtqiaplus","trans","transgender","queer","ace","asexual","bi","bisexual","pan","pansexual","lesbian","homosexual","homo","nb","enby","nonbinary","glbt","glbtq","rainbow","pride"]
-
-def generate():
-    while True:
-        end = input("Text: ")
-        new = ["#"+word+end for word in l]
-        print(" ".join(new))
-
-generate()
