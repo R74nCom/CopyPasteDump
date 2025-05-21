10f49ca78f94fe29a471a67d4d6a259797fda161
slweeb <91897291+slweeb@users.noreply.github.com>
2025-05-21 14:47:28 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+/*R74n Observer
+  Don't worry, I'm harmless!*/
+if (document.referrer && document.referrer.indexOf("r74n.") === -1) {
+	const refdomain = document.referrer.replace(/^https?:\/\//g, "").replace(/^www\./g, "").replace(/\/$/g, "");
+    document.cookie = "R74nRef="+refdomain+";max-age=86400;path=/;domain=r74n.com";
+}
+
 function copy(that){
 var inp =document.createElement('input');
 document.body.appendChild(inp)
